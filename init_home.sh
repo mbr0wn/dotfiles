f855d438a973ec1b5267983315a38aa0bb1331ec
Martin Braun <martin.braun@ettus.com>
2016-01-02 20:59:35 -0800

    Updated init_home script, fixed bugs

--- init_home.sh
+++ init_home.sh
@@ -25,30 +25,30 @@ do
 	echo "Linking $i to ~/.$i ..."
 	ln -s ${CWD}/home_symlink/$i ~/.$i
 done
-exit
 
 ## Copy dotfiles
-echo"Copying other dotfiles to home dir..."
+echo "== Copying other dotfiles to home dir..."
 HOME_COPY_FILES=`(cd home_copy; ls)`
 for i in ${HOME_COPY_FILES}
 do
 	check_and_backup $i
 	echo "Copying $i to ~/.$i ..."
-	cp ${CWD}/home_symlink/$i ~/.$i
+	cp ${CWD}/home_copy/$i ~/.$i
 done
 
 ## Clone important repos
 cd ~
+echo "== Cloning vim config..."
 check_and_backup .vim
 check_and_backup .vimrc
 git clone --recursive git@github.com:mbr0wn/vimrc.git .vim
 ln -s .vim/vimrc .vimrc
 
+echo "== Cloning zsh config..."
 check_and_backup .oh-my-zsh
 git clone --recursive git@github.com:mbr0wn/oh-my-zsh.git .oh-my.zsh
-
 if [ -x /bin/zsh ]; then
-	echo "Selecting zsh as default shell..."
+	echo "Selecting zsh as default shell (will ask for password)..."
 	chsh -s /bin/zsh
 else
 	echo "zsh not found/installed. Not running chsh."
